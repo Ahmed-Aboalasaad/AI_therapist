35d811e223190cea12f3de0244a1b6fa9828a0a3
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2026-06-01 14:34:07 +0300

    Modify .gitignore

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
     from language_detector import CharNGramSVMDetector
     lang_detector = CharNGramSVMDetector()
-    predictions = lang_detector.predict(["This is a test sentence.", "C'est une phrase de test."])
+    predictions = lang_detector.predict(["صبح يبوعمو", "Hi"])
     print(predictions)
