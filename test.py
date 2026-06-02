5d5274e9a72966f57a706c0d7522456d6722f7ee
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2026-06-02 10:17:44 +0300

    Omit redundant files from old main branch

--- test.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    from language_detector import CharNGramSVMDetector
-    lang_detector = CharNGramSVMDetector()
-    predictions = lang_detector.predict(["صبح يبوعمو", "Hi"])
-    print(predictions)
